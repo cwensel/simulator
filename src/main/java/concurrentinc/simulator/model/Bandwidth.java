a3d533382e7d867b5e30820b5c50d616fd2c631c
Chris K Wensel <chris@wensel.net>
2011-12-06 13:37:34 -0800

    remove flatten method

--- src/main/java/concurrentinc/simulator/model/Bandwidth.java
+++ src/main/java/concurrentinc/simulator/model/Bandwidth.java
@@ -7,17 +7,24 @@
 package concurrentinc.simulator.model;
 
 /** Nominal bandwidth for common interfaces. */
-public interface Bandwidth
+public class Bandwidth
   {
-  long MB = 1;
-  long GB = MB * 1024;
-  long TB = GB * 1024;
+  public static long BytesInMb = 1 * 1024 * 1024;
+
+  public static long MB = 1;
+  public static long GB = MB * 1024;
+  public static long TB = GB * 1024;
 
   // see http://en.wikipedia.org/wiki/List_of_device_bit_rates
 
-  double Megabit1 = 1.25 * MB; // 10BASE-X
-  double Megabit10 = 12.5 * MB; // 100BASE-X
-  double Gigabit1 = 125 * MB; // 1000BASE-X
-  double Gigabit10 = 1.25 * GB; // 10GBASE-X
-  double Gigabit100 = 12.5 * GB; // 100GBASE-X
+  public static double Megabit1 = 1.25 * MB; // 10BASE-X
+  public static double Megabit10 = 12.5 * MB; // 100BASE-X
+  public static double Gigabit1 = 125 * MB; // 1000BASE-X
+  public static double Gigabit10 = 1.25 * GB; // 10GBASE-X
+  public static double Gigabit100 = 12.5 * GB; // 100GBASE-X
+
+  public static double toMbFromBytes( double bytes )
+    {
+    return bytes / BytesInMb;
+    }
   }
